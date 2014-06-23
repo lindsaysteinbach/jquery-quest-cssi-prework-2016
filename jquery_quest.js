2c8523d032386b0554022869e8878bf4e3672e0c
Spencer Rogers <spencer1248@gmail.com>
2014-06-22 20:15:34 -0400

    hover

--- jquery_quest.js
+++ jquery_quest.js
@@ -1,2 +1,29 @@
-// Your answers go here!
-// Try to keep them organized as you're going!
+//##############################################################################
+// jQuery
+//##############################################################################
+
+//##############################################################################
+// jQuery and Events
+//##############################################################################
+
+$('#click-target').on('click', function(e) {
+  // This is the DOM element that was clicked
+  var $target = $(e.target);
+});
+
+$('.letter-b').on('hover',
+  function(e) {
+    var $target = $(e.target);
+    $target.css('font-size', '36px');
+    console.log('hover in');
+  },
+  function(e) {
+    var $target = $(e.target);
+    $target.css('font-size', '34px');
+    console.log('hover out');
+  }
+);
+
+//##############################################################################
+// jQueryUI
+//##############################################################################
