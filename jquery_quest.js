a782aab29a1c3f1e8f4fd21de10e281dedf47658
lindsaysteinbach <lsteinbach@google.com>
2016-05-15 16:44:31 -0400

    Done.

--- jquery_quest.js
+++ jquery_quest.js
@@ -2,7 +2,23 @@
 // jQuery
 //##############################################################################
 
-// Your code goes here!
+$('#color-target').css("color","green");
+
+$('#move-target').css({
+  position: 'relative',
+  top: '+15px',
+  left: '+15px'
+});
+
+$('#prepend-target').prepend("<");
+
+if (($('#condition-target').text() % 2) == 0) {
+  $('#condition-target').css("color","blue")
+}
+else {
+  $('#condition-target').css("color","purple")
+}
+
 
 //##############################################################################
 // jQuery and Events
@@ -12,12 +28,12 @@ $('#click-target').on('click', function(e) {
   // This is the DOM element that was clicked
   var $target = $(e.target);
 
-  // Your code goes here!
+  $target.toggleClass('coolness');
 });
 
 $('.letter-b').hover(
-  function(e) { 'your code goes here!' },
-  function(e) { 'your code goes here!' }
+  function(e) { var $target = $(e.target); $target.addClass('letter-b') },
+  function(e) { var $target = $(e.target); $target.removeClass('letter-b') }
 );
 
 //##############################################################################
@@ -25,3 +41,9 @@ $('.letter-b').hover(
 //##############################################################################
 
 // Your code goes here!
+
+$('#accordion').accordion();
+
+$('#flying-kid').draggable({
+  addClasses: false
+});
